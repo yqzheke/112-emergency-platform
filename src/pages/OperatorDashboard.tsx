7dc5ba779ce20323f92353a32d464475ac559a65
Olzhas Amankeldin <olzhasikexray@gmail.com>
2026-09-02 23:09:43 +0500

    Fix ResQ operator build

--- src/pages/OperatorDashboard.tsx
+++ src/pages/OperatorDashboard.tsx
@@ -335,34 +335,6 @@ function ClockIcon({
   )
 }
 
-function LocationIcon({
-  size = 18,
-}: IconProps) {
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      aria-hidden="true"
-    >
-      <path
-        d="M20 10c0 5-8 11-8 11S4 15 4 10a8 8 0 1 1 16 0Z"
-        stroke="currentColor"
-        strokeWidth="1.8"
-      />
-
-      <circle
-        cx="12"
-        cy="10"
-        r="2.5"
-        stroke="currentColor"
-        strokeWidth="1.8"
-      />
-    </svg>
-  )
-}
-
 function scrollToSection(id: string) {
   document
     .getElementById(id)
